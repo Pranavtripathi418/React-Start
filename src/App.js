0f00a7f76a3d9e17cb942e6d1619f40e9dbbcd7f
pranavtripathi418 <pranavtripathi418@gmail.com>
2024-10-16 02:14:41 +0530

    creating Reusable components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenses=[
+    {id:1, date:new Date(2023,7,15), location: 'Mumbai', title:'Book', price:50},
+    {id:2, date:new Date(2022,5,25), location: 'Pune', title:'Insurance', price:200},
+    {id:3, date:new Date(2021,9,6), location: 'Bangalore', title:'Laptop', price:700},
+    {id:4, date:new Date(2026,7,1), location: 'Delhi', title:'Pencil', price:500}
+  ]
   return (
     <div>
     <h1>Let's Get Started</h1>
-    <ExpenseItem></ExpenseItem>
+    {expenses.map((expense)=>{
+      return (
+        <ExpenseItem 
+        date={expense.date}
+        location={expense.location} 
+        title={expense.title}
+        price={expense.price}
+        ></ExpenseItem>
+      )
+    })}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,14 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(){
-    const expenseDate = new Date(2024,7,15).toISOString();
-    const expenseTitle = 'Insurance';
-    const expenseAmount= 50;
+function ExpenseItem(props){
+   
 return (
 <div className="expense-item "> 
-    <div>{expenseDate}</div>
-    <div className="expense-item__location">Delhi</div>
+    <div>{props.date.toISOString()}</div>
+    <div className="expense-item__location">{props.location}</div>
     <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
     </div>
 </div>
 );
