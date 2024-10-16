7b761ddadc3f7c541abb3b097933ed0fdbfe7fc6
pranavtripathi418 <pranavtripathi418@gmail.com>
2024-10-17 00:50:41 +0530

    Splittig the components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,20 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props){
+
+
    
 return (
-<div className="expense-item "> 
-    <div>{props.date.toISOString()}</div>
+<Card className="expense-item "> 
+    <ExpenseDate date={props.date}/>
     <div className="expense-item__location">{props.location}</div>
     <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
     </div>
-</div>
+</Card>
 );
 
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,27 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css"
+import Card from "./Card";
+
+function Expenses(props){
+    const expenses=props.items;
+
+    return(
+        <Card className="expenses">
+{expenses.map((expense)=>{
+        return ( 
+          <ExpenseItem 
+          key={expense.id}
+          date={expense.date}
+          location={expense.location} 
+          title={expense.title}
+          price={expense.price}
+          />
+        )
+      })}
+        </Card>
+    )
+
+      
+}
+
+export default Expenses;
