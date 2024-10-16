48bf21f3ae7bcb72fea644c1981d4187b8638474
pranavtripathi418 <pranavtripathi418@gmail.com>
2024-10-17 00:53:12 +0530

    Some styling changes

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,30 @@
+import "./ExpenseDate.css"
+
+function ExpenseDate(props){
+    const months=[
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+
+    const month= months[props.date.getMonth()];
+   const year= props.date.getFullYear();
+   const day= props.date.getDay();
+
+   return(<div className="expense-date">
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__year">{year}</div>
+        <div className="expense-date__day">{day}</div>
+    </div>)
+}
+
+export default ExpenseDate;
